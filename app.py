160fb3f6f5e7ce9a2f2d7fc88eee4cfacf5cad00
LAPTOP-5EEBDO2M\uesr <orzrzlyo@gmail.com>
2018-08-22 14:54:31 +0900

    작동가능하게 수정

--- app.py
+++ app.py
@@ -6,16 +6,33 @@ app = Flask(__name__)
 app.debug = True
 
 
+def get_word(definition=True):
+    df = pd.read_csv('words.csv')
+
+    # 단어 랜덤 선택
+    word = random.choice(df['word'])
+    meaning = df[df['word'] == word]['meaning'].values[0]
+
+    # 다른 보기 만들기
+    other = random.sample(set(df[df['meaning'] != meaning]['meaning']), 3)
+    other.append(meaning)
+    if definition:
+        return word, meaning
+    else:
+        return word, other
+
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    word, other = get_word(definition=False)
+    print(other)
+    return render_template('home.html', word=word, other=other)
 
 
-# @app.route('/about')
-# def hello_world():
-#     df = pd.read_csv('words.csv')
-#     word = random.choice(df['word'])
-#     return word
+@app.route('/study')
+def study():
+    word, meaning = get_word(True)
+    return render_template('study.html', word=word, meaning=meaning)
 
 
 if __name__ == '__main__':
